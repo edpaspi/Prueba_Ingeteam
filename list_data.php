fcbce2516d2f7a7293c4895ab1641419ce20e56c
edpaspi <edupastorpicazo@gmail.com>
2020-08-07 10:55:33 +0200

    Update list_data.php

--- list_data.php
+++ list_data.php
@@ -6,6 +6,11 @@
 <body>
 	<p>
 	<?php 
+	//Set DB configuration
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "ingeteam_prueba";
 
 	echo "<table style='border: solid 1px black;'>";
 	echo "<tr><th>Name</th><th>E-Mail</th><th>Description</th><th>Address</th><th>Postal Code</th></tr>";
@@ -27,45 +32,33 @@
 	    echo "</tr>" . "\n";
 	  }
 	} 
-	//
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$dbname = "ingeteam_prueba";
+		
+	try {
+	    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+	    // set the PDO error mode to exception
+	    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	    //Insert user data
+		$sql = "SELECT * FROM TB_USER_DATA";
+		$stmt = $conn->prepare($sql);
+		$stmt->execute();
 
-	/*// Create connection
-	$conn = new mysqli($servername, $username, $password, $dbname);
-	// Check connection
-	if ($conn->connect_error) {
-	  die("Connection failed: " . $conn->connect_error);
-	  echo "Fallo en conexion";
-	} */
-	  	try {
-		    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-		    // set the PDO error mode to exception
-		    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		    //Insert user data
-		  	$sql = "SELECT * FROM TB_USER_DATA";
-			$stmt = $conn->prepare($sql);
-			$stmt->execute();
-
-			$result = $stmt->setFetchMode(PDO::FETCH_ASSOC); 
-			foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
-    			echo $v;
-  			}
-			/*if ($result->num_rows > 0) {
-			  // output data of each row
-			  while($row = $result->fetch_assoc()) {
-			    echo "Name: " . $row["USER_NAME"]. " - E-mail: " . $row["EMAIL"]. " - DESCRIPTION" . $row["DESCRIPTION"]. " - ADDRESS" . $row["ADDRESS"]. " - POSTAL_CODE" . $row["POSTAL_CODE"]. "<br>";
-			  }
-			} else {
-			  echo "0 results";
-			}*/
-		} catch(PDOException $e) {
-			echo $sql . "<br>" . $e->getMessage();
+		$result = $stmt->setFetchMode(PDO::FETCH_ASSOC); 
+		foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+		echo $v;
 		}
+		/*if ($result->num_rows > 0) {
+		  // output data of each row
+		  while($row = $result->fetch_assoc()) {
+		    echo "Name: " . $row["USER_NAME"]. " - E-mail: " . $row["EMAIL"]. " - DESCRIPTION" . $row["DESCRIPTION"]. " - ADDRESS" . $row["ADDRESS"]. " - POSTAL_CODE" . $row["POSTAL_CODE"]. "<br>";
+		  }
+		} else {
+		  echo "0 results";
+		}*/
+	} catch(PDOException $e) {
+		echo $sql . "<br>" . $e->getMessage();
+	}
 
-		$conn = null;
+	$conn = null;
 	?>
 	</p>
 </body>
